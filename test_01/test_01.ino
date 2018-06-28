8fbe539a8785655644b5338b94704eaa67d8ab35
Unknown <barker.j.eric@gmail.com>
2018-06-27 19:13:34 -0600

    Test file for arduino
    Beginning of state machine

---
+++ test_01/test_01.ino
@@ -0,0 +1,23 @@
+void setup() {
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+
+  delay(500);
+  
+  digitalWrite(6,LOW);
+  digitalWrite(5,HIGH);
+
+  delay(500);
+
+
+  
+
+}
