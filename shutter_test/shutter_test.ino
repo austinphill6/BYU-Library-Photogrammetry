ee39d63ac255e9379f52ed6e72a859b8fa9f0c83
Austin Phillips <a.s.phillips@hotmail.com>
2018-07-19 19:23:14 -0600

    final changes to shutter test

--- shutter_test/shutter_test.ino
+++ shutter_test/shutter_test.ino
@@ -1,9 +1,20 @@
+#define FOCUS 2
+#define SHUTTER 3
+
 void setup()
 {
-  
+  pinMode(FOCUS,OUTPUT);
+  pinMode(SHUTTER,OUTPUT);
 }
 
 void loop()
 {
-  
+  delay(500);
+  digitalWrite(FOCUS,HIGH);
+  delay(500);
+  digitalWrite(SHUTTER,HIGH);
+  delay(100);
+  digitalWrite(SHUTTER,LOW);
+  digitalWrite(FOCUS,LOW);
+  delay(10000);
 }
